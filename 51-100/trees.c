24dab0ab79e813a0db23f17254c3c641dc72183b
Gilberto Cunha <gcacademic@outlook.pt>
2020-05-19 23:51:48 +0100

    Finish tree functions

--- 51-100/trees.c
+++ 51-100/trees.c
@@ -215,18 +215,255 @@ int min (int a, int b) {
   return r;
 }
 
-// 37: ???
+// 37: 10/10
 int iguaisAB (ABin a, ABin b) {
   int r;
-  if (a == NULL && b == NULL) r = 0;
-  else if (a == NULL || b == NULL) r = -1;
+  if (a == NULL && b == NULL) r = 1;
+  else if (a == NULL || b == NULL) r = 0;
   else if (a->valor == b->valor) {
     int r_esq, r_dir;
     r_dir = iguaisAB (a->dir, b->dir);
     r_esq = iguaisAB (a->esq, b->esq);
     r = min (r_dir, r_esq);
   }
-  else r = -1;
+  else r = 1;
+
+  return r;
+}
+
+// 38: 10/10
+LInt nivelL (ABin a, int n) {
+  LInt r;
+  if (a == NULL) r = NULL;
+  else if (n == 1) {
+    r = malloc (sizeof (struct lista));
+    r->valor = a->valor;
+    r->prox = NULL;
+  }
+  else {
+    LInt r_dir;
+    r = nivelL (a->esq, n-1);
+    r_dir = nivelL (a->dir, n-1);
+    r = concatL (r, r_dir);
+  }
+
+  return r;
+}
+
+// 39: 10/10
+int nivelV (ABin a, int n, int v[]) {
+  int r;
+  if (a == NULL) r = 0;
+  else if (n == 1) {
+    v[0] = a->valor;
+    r = 1;
+  }
+  else {
+    r = nivelV (a->esq, n-1, v);
+    r += nivelV (a->dir, n-1, v+r);
+  }
+
+  return r;
+}
+
+// 40: 10/10
+int dumpABin (ABin a, int v[], int N) {
+  int r;
+  if (a == NULL || N == 0) r = 0;
+  else {
+    r = dumpABin (a->esq, v, N);
+    if (r < N) {
+      v[r] = a->valor;
+      ++r;
+    }
+    if (r < N) r += dumpABin (a->dir, v+r, N-r);
+  }
+
+  return r;
+}
+
+int somaABin (ABin a) {
+  int r;
+  if (a == NULL) r = 0;
+  else r = a->valor + somaABin (a->esq) + somaABin (a->dir) ;
+
+  return r;
+}
+
+// 41: 10/10
+ABin somasAcA (ABin a) {
+  ABin r;
+  if (a == NULL) r = NULL;
+  else {
+    r = malloc (sizeof (struct nodo));
+    r->valor = somaABin (a);
+    r->esq = somasAcA (a->esq);
+    r->dir = somasAcA (a->dir);
+  }
+
+  return r;
+}
+
+// 42: 10/10
+int contaFolhas (ABin a) {
+  int r;
+  if (a == NULL) r = 0;
+  else if (a->dir == NULL && a->esq == NULL) r = 1;
+  else r = contaFolhas (a->esq) + contaFolhas (a->dir);
+
+  return r;
+}
+
+// 43: 10/10
+ABin cloneMirror (ABin a) {
+  ABin r;
+  if (a == NULL) r = NULL;
+  else {
+    r = malloc (sizeof (struct nodo));
+    r->valor = a->valor;
+    r->esq = cloneMirror (a->dir);
+    r->dir = cloneMirror (a->esq);
+  }
+
+  return r;
+}
+
+int addOrd_rec (ABin *a, int x) {
+  int r;
+
+  if (*a == NULL) {
+    *a = malloc (sizeof (struct nodo));
+    (*a)->valor = x;
+    (*a)->esq = (*a)->dir = NULL;
+    r = 0;
+  }
+  else if (x < a->valor) r = addOrd (&((*a)->esq), x);
+  else if (x == a->valor) r = 1;
+  else r = addOrd (&((*a)->dir), x);
+
+  return r;
+}
+
+// 44: 10/10
+int addOrd (ABin *a, int x) {
+  int r;
+
+  while (*a != NULL && (*a)->valor != x) {
+    if (x < (*a)->valor) a = &((*a)->esq);
+    else a = &((*a)->dir);
+  }
+
+  if (*a == NULL) {
+    r = 0;
+    *a = malloc (sizeof (struct nodo));
+    (*a)->valor = x;
+    (*a)->esq = (*a)->dir = NULL;
+  }
+  else r = 1;
+
+  return r;
+}
+
+// 45: 10/10
+int lookupAB (ABin a, int x) {
+  int r;
+
+  while (a != NULL && a->valor != x) {
+    if (x < a->valor) a = a->esq;
+    else a = a->dir;
+  }
+
+  if (a == NULL) r = 0;
+  else r = 1;
+
+  return r;
+}
+
+// 46: 10/10
+int depthOrd (ABin a, int x) {
+  int r = 1;
+
+  while (a != NULL && a->valor != x) {
+    if (x < a->valor) a = a->esq;
+    else a = a->dir;
+    ++r;
+  }
+
+  if (a == NULL) r = -1;
+
+  return r;
+}
+
+// 47: 10/10
+int maiorAB (ABin a) {
+  int r;
+
+  while (a->dir != NULL) a = a->dir;
+
+  r = a->valor;
+
+  return r;
+}
+
+// 48: 10/10
+void removeMaiorA (ABin *a) {
+  while ((*a) != NULL && (*a)->prox != NULL)
+    a = &((*a)->dir);
+
+  if (*a != NULL) {
+    ABin tmp = (*a)->esq;
+    free (*a);
+    *a = tmp;
+  }
+}
+
+// 49: 10/10
+int quantosMaiores (ABin a, int x) {
+  int r;
+
+  if (a == NULL) r = 0;
+  else if (a->valor <= x) r = quantosMaiores (a->dir, x);
+  else r = 1 + quantosMaiores (a->esq, x) + quantosMaiores (a->dir, x);
+
+  return r;
+}
+
+// 50: 10/10
+void listToBTree (LInt l, ABin *a) {
+  while (l != NULL) {
+    *a = insere (*a, l->valor);
+    l = l->prox;
+  }
+}
+
+int quantosMenores (ABin a, int x) {
+  int r;
+
+  if (a == NULL) r = 0;
+  else if (a->valor >= x) r = quantosMenores (a->esq, x);
+  else r = 1 + quantosMenores (a->esq, x) + quantosMenores (a->dir, x);
+
+  return r;
+}
+
+// 51: 10/10
+int deProcura (ABin a) {
+  int r;
+
+  if (a == NULL) r = 1;
+  else {
+    int menores, maiores;
+    menores = quantosMenores (a->dir, x);
+    maiores = quantosMaiores (a->esq, x);
+
+    int r_dir, r_esq;
+    r_dir = deProcura (a->dir);
+    r_esq = deProcura (a->esq);
+
+    if (maiores == 0 && menores == 0)
+      r = min (r_dir, r_esq);
+    else r = 0;
+  }
 
   return r;
 }
